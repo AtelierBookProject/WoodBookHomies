7613ae88e1dd33fa466119f32ffe2f69dd6db7c7
FlawedD3sign <57969207+FlawedD3sign@users.noreply.github.com>
2019-12-01 17:15:54 -0500

    Update script.js

--- Person1stuff/script.js
+++ Person1stuff/script.js
@@ -1,14 +1,33 @@
+let dataServer;
+let pubKey = 'pub-c-9e29a8c5-0f63-44a0-a5e1-5e18e8cd3c93';
+let subKey = 'sub-c-97a10462-10aa-11ea-b660-563e374d08f6';
+
+
+
 let r = 10;//radius
 let a = 0;//polar angle
 let c = 20;// makes space between shapes
 let angle =0;
 let art;
 
+let WoodShape = "shape";
+
+
 function setup() {
   createCanvas(400, 400, WEBGL);
  //background(175);
   //img = loadImage('WoodBoi.jpg');
-  art= createGraphics(400,400);
+art = createGraphics(400,400);
+dateServer = new PubNub(
+  {
+publish_key : pubKey,
+subscribe_key : subKey,
+SSL : true
+  });
+
+dataServer.addListener({message: readIncoming});
+dataServer.subscribe({channels: [WoodShape]});
+
 }
 
 function draw() {
@@ -36,4 +55,31 @@ function draw() {
   
   angle+=0.03;
   pop();
+}
+
+///uses built in mouseClicked function to send the data to the pubnub server
+function mouseClicked() {
+ 
+
+  // Send Data to the server to draw it in all other canvases
+  dataServer.publish(
+    {
+      channel: WoodShape,
+      message: 
+      {
+        messageText: "This is Just a test Bois"       //get the value from the text box and send it as part of the message   
+      }
+    });
+
+}
+
+function readIncoming(inMessage) //when new data comes in it triggers this function, 
+{                               // this works becsuse we subscribed to the channel in setup()
+  
+  // simple error check to match the incoming to the channelName
+  if(inMessage.channel == WoodShape)
+  {
+
+    console.log(inMessage);
+  }
 }
